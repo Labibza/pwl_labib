337803f15bfb802c2e69ae8374a1af953af95dd6
Labib <111202315339@mhs.dinus.ac.id>
2025-06-10 00:15:13 +0700

    update Minggu 10

--- belajar-ci/app/Controllers/ProdukController.php
+++ belajar-ci/app/Controllers/ProdukController.php
@@ -2,13 +2,45 @@
 
 namespace App\Controllers;
 
-use App\Controllers\BaseController;
-use CodeIgniter\HTTP\ResponseInterface;
+use App\Models\ProductModel;
 
 class ProdukController extends BaseController
 {
+    protected $product; 
+
+    function __construct()
+    {
+        $this->product = new ProductModel();
+    }
+
     public function index()
     {
-        return view('v_produk');
+        $product = $this->product->findAll();
+        $data['product'] = $product;
+
+        return view('v_produk', $data);
     }
+
+    public function create()
+    {
+        $dataFoto = $this->request->getFile('foto');
+
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah'),
+            'created_at' => date("Y-m-d H:i:s")
+        ];
+
+        if ($dataFoto->isValid()) {
+            $fileName = $dataFoto->getRandomName();
+            $dataForm['foto'] = $fileName;
+            $dataFoto->move('img/', $fileName);
+        }
+
+        $this->product->insert($dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Ditambah');
+    } 
+
 }
