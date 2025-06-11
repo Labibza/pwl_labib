86d9a7ad0935d785fff9321e4cbca8c9e8f2e714
Labib <111202315339@mhs.dinus.ac.id>
2025-06-12 01:35:39 +0700

    update Minggu-10

--- belajar-ci/app/Controllers/ProdukController.php
+++ belajar-ci/app/Controllers/ProdukController.php
@@ -43,4 +43,47 @@ class ProdukController extends BaseController
         return redirect('produk')->with('success', 'Data Berhasil Ditambah');
     } 
 
+    public function edit($id)
+    {
+        $dataProduk = $this->product->find($id);
+
+        $dataForm = [
+            'nama' => $this->request->getPost('nama'),
+            'harga' => $this->request->getPost('harga'),
+            'jumlah' => $this->request->getPost('jumlah'),
+            'updated_at' => date("Y-m-d H:i:s")
+        ];
+
+        if ($this->request->getPost('check') == 1) {
+            if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+                unlink("img/" . $dataProduk['foto']);
+            }
+
+            $dataFoto = $this->request->getFile('foto');
+
+            if ($dataFoto->isValid()) {
+                $fileName = $dataFoto->getRandomName();
+                $dataFoto->move('img/', $fileName);
+                $dataForm['foto'] = $fileName;
+            }
+        }
+
+        $this->product->update($id, $dataForm);
+
+        return redirect('produk')->with('success', 'Data Berhasil Diubah');
+    }
+
+    public function delete($id)
+    {
+        $dataProduk = $this->product->find($id);
+
+        if ($dataProduk['foto'] != '' and file_exists("img/" . $dataProduk['foto'] . "")) {
+            unlink("img/" . $dataProduk['foto']);
+        }
+
+        $this->product->delete($id);
+
+        return redirect('produk')->with('success', 'Data Berhasil Dihapus');
+    }
+
 }
